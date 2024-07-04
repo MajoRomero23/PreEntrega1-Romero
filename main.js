fba0664254d0c47c9e74f4b41a404313393078fb
Maria Romero <majo_pozos_23@hotmail.com>
2024-07-04 11:47:45 -0600

    Segunda entrega

--- main.js
+++ main.js
@@ -1,58 +1,54 @@
 let nombreCliente = prompt ('Hola que tal, por favor ingresa tu nombre');
 alert ("Hola "+nombreCliente+" muchas gracias por visitar nuestro cine");
 
-let pelicula = '';
+let peliculaId = '';
 let cantidadBoletos=0;
-let areaSala='';
 let seguirComprando = false;
-let precioBoletos = 0;
 let precioPagar = 0; 
 let cantidadTotal=0;
 
+//array con id de películas y su sinópsis
+const peliculas = [
+    {id: 1, nombre: "Intensamente", sinopsis: "Sinópsis: La película de Disney y Pixar INTENSA-MENTE 2 regresa a la mente de la recién adolescente Riley justo cuando el cuartel general está sufriendo una repentina demolición para hacer sitio a algo totalmente inesperado: ¡Nuevas emociones! Alegría, Tristeza, Furia, Temor y Desagrado, que llevan mucho tiempo llevando a cabo una operación exitosa, no están seguros de cómo sentirse cuando aparece Ansiedad. Y parece que no está sola"},
+    {id: 2, nombre: "Mi villano favorito", sinopsis: "Sinópsis: Gru, Lucy, Margo, Edith y Agnes le dan la bienvenida a un nuevo miembro a la familia, Gru Jr., quien está decidido a atormentar a su papá. Gru se enfrenta a un nuevo némesis llamado Maxime Le Mal y su novia Valentina, y la familia se ve obligada a huir"},
+    {id: 3, nombre: "Bad Boys: Hasta la Muerte", sinopsis: "Sinópsis: Este verano, los chicos malos favoritos del mundo están de regreso con su icónica mezcla de vertiginosa acción y escandalosa comedia, pero esta vez con un giro: los detectives favoritos de Miami son fugitivos."},
+    ];
+
+//areas de la sala con id, nombres y precios
+const areaSala = [
+    {id: 1, nombre: "Frente a la pantalla", precio: 200},
+    {id: 2, nombre: "En el centro de la sala", precio: 300},
+    {id: 3, nombre: "Al fondo de la sala", precio: 400},
+    ];
+    
 do {
-    let pelicula;
     do {
-        pelicula = parseInt(prompt("Qué película deseas ver el día de hoy?  \n 1: Intensamente 2  \n 2: Mi villano favorito 4  \n 3:Bad Boys:Hasta la Muerte"));
-    } while (pelicula !== 1 && pelicula !== 2 && pelicula !== 3);    
+        peliculaId = parseInt(prompt("Qué película deseas ver el día de hoy?  \n 1: Intensamente 2  \n 2: Mi villano favorito 4  \n 3:Bad Boys:Hasta la Muerte"));
+        if(![1,2,3].includes(peliculaId)){ 
+            alert("Por favor ingresa una película válida (1,2 o 3)");
+        }
+    } while (![1,2,3].includes(peliculaId));    
 
-switch (pelicula) {
-    case 2:
-        alert("Sinópsis: Gru, Lucy, Margo, Edith y Agnes le dan la bienvenida a un nuevo miembro a la familia, Gru Jr., quien está decidido a atormentar a su papá. Gru se enfrenta a un nuevo némesis llamado Maxime Le Mal y su novia Valentina, y la familia se ve obligada a huir");
-        break;
-    case 3:
-        alert("Sinópsis: Este verano, los chicos malos favoritos del mundo están de regreso con su icónica mezcla de vertiginosa acción y escandalosa comedia, pero esta vez con un giro: los detectives favoritos de Miami son fugitivos.");
-        break;
-    case 1:
-        alert("Sinópsis: La película de Disney y Pixar INTENSA-MENTE 2 regresa a la mente de la recién adolescente Riley justo cuando el cuartel general está sufriendo una repentina demolición para hacer sitio a algo totalmente inesperado: ¡Nuevas emociones! Alegría, Tristeza, Furia, Temor y Desagrado, que llevan mucho tiempo llevando a cabo una operación exitosa, no están seguros de cómo sentirse cuando aparece Ansiedad. Y parece que no está sola.");
-        break;
-    }
+    let peliculaSeleccionada = peliculas.find(movie => movie.id === peliculaId);
+    alert("Sinópsis: " + peliculaSeleccionada.sinopsis);
 
-let cantidadBoletos;
 do {
     cantidadBoletos = parseInt(prompt("Ya seleccionaste tu película, ¿Cuántos boletos deseas comprar?"));
 } while (isNaN(cantidadBoletos)|| cantidadBoletos < 1 || cantidadBoletos >= 10);
 
-let areaSala;
 do {
-    areaSala = parseInt(prompt("¿En qué área de la sala deseas ubicarte durante tu película? \n 1:Frente a la pantalla \n 2:En el centro de la sala \n 3:Al fondo de la sala"));
-} while (areaSala !== 1 && areaSala !== 2 && areaSala !== 3);
+    areaSalaId = parseInt(prompt("¿En qué área de la sala deseas ubicarte durante tu película? \n 1: Frente a la pantalla \n 2: En el centro de la sala \n 3: Al fondo de la sala"));
+    if(![1,2,3].includes(areaSalaId)){ 
+        alert("Por favor selecciona un área válida (1,2 o 3)");
+    }
+} while (![1,2,3].includes(areaSalaId));  
 
-let precioBoletos=0;
-switch (areaSala) {
-    case 1:
-        precioBoletos=200;
-        break;
-    case 2:
-        precioBoletos=300;
-        break;
-    case 3:
-        precioBoletos=400;
-        break;
-    default:
-        alert('El área de la sala seleccionada no es válida');
-        continue;
-}
-precioPagar += precioBoletos*cantidadBoletos;
+let areaSeleccionada = areaSala.find(areas => areas.id === areaSalaId);
+console.log (areaSeleccionada);
+
+
+let precioBoletos = areaSeleccionada.precio * cantidadBoletos;
+precioPagar += precioBoletos;
 cantidadTotal += cantidadBoletos;
 
 seguirComprando = confirm('¿Deseas comprar entradas para otra película?');
